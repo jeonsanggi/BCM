0d5ef33c6e196e56b4c4ee8b0e670a916cc7db62
byunseonuk <byunseonuk@naver.com>
2018-05-02 21:38:06 +0900

    2018=05-02.1

--- app/src/main/java/com/example/lg/bcm/SignUp.java
+++ app/src/main/java/com/example/lg/bcm/SignUp.java
@@ -1,7 +1,6 @@
 package com.example.lg.bcm;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,125 +24,109 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by LG on 2018-04-17.
+ * Created by byunseonuk on 2018-04-20.
  */
 
-public class Login extends AppCompatActivity{
+public class SignUp extends AppCompatActivity {
     private static String TAG = "phptest_MainActivity";
 
-    private static final String TAG_ID = "id";
-    private static final String TAG_COMPANY = "company";
-    private static final String TAG_NAME = "name";
-    private static final String TAG_PHONE = "phone";
-    private static final String TAG_TEL = "tel";
-    private static final String TAG_EMAIL = "email";
-    private static final String TAG_ADDRESS ="address";
-    private static final String TAG_JSON="logincheck";
+    private EditText mEdittextId;
+    private EditText mEditPassword;
+    private EditText mEditTextCompany;
+    private EditText mEditTextName;
+    private EditText mEditTextPhone;
+    private EditText mEditTextTel;
+    private EditText mEditTextEmail;
+    private EditText mEditTextAddress;
+    private String imgurl;
+    private Button signup_bt;
+    private static final String TAG_JSON="signupcheck";
     private static final String TAG_CHECK="check";
 
-
-    private EditText User_ID;
-    private EditText User_PW;
-
-    private TextView mTextViewResult;
-    ArrayList<HashMap<String, String>> mArrayList;
-   ListView mlistView;
+    String company;
+    String android_num;
+    String phone;
+    String tel;
+    String email;
+    String address;
     String mJsonString;
-    String user_id;
-    String name;
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.sign_up);
+        mEdittextId = (EditText)findViewById(R.id.signup_id);
+        mEditPassword = (EditText)findViewById(R.id.signup_password);
+        mEditTextCompany = (EditText)findViewById(R.id.signup_company);
+        mEditTextName = (EditText)findViewById(R.id.signup_name);
+        mEditTextPhone = (EditText)findViewById(R.id.signup_phone);
+        mEditTextTel = (EditText)findViewById(R.id.signup_tel);
+        mEditTextEmail = (EditText)findViewById(R.id.signup_email);
+        mEditTextAddress = (EditText)findViewById(R.id.signup_address);
 
-        User_ID = (EditText)findViewById(R.id.editText_login_id);
-        User_PW = (EditText)findViewById(R.id.editText_login_pw);
 
-        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
-        //mlistView = (ListView) findViewById(R.id.listView_main_list);
-        mArrayList = new ArrayList<>();
-
-        Button buttonInsert = (Button)findViewById(R.id.button_main_login);
+        Button buttonInsert = (Button)findViewById(R.id.signup_bt);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user_id = User_ID.getText().toString();
-                String user_pw = User_PW.getText().toString();
+                String id = mEdittextId.getText().toString();
+                String password = mEditPassword.getText().toString();
+                String company = mEditTextCompany.getText().toString();
+                String name = mEditTextName.getText().toString();
+                String phone = mEditTextPhone.getText().toString();
+                String tel = mEditTextTel.getText().toString();
+                String email = mEditTextEmail.getText().toString();
+                String address = mEditTextAddress.getText().toString();
 
-                Login.GetData task = new Login.GetData();
-                task.execute(user_id, user_pw);
-
-                User_ID.setText("");
-                User_PW.setText("");
-
-            }
-        });
-        Button buttonsignup = (Button)findViewById(R.id.button_main_sign_up);
-        buttonsignup.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(Login.this,SignUp.class);
-                startActivity(intent);
-                finish();
+                Insert task = new Insert();
+                task.execute(id,password,company,name,phone,tel,email,address);
             }
         });
     }
-
-
-    private class GetData extends AsyncTask<String, Void, String>{
+    class Insert extends AsyncTask<String, Void, String> {
         ProgressDialog progressDialog;
-        String errorString = null;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
-            progressDialog = ProgressDialog.show( Login.this,
+            progressDialog = ProgressDialog.show(SignUp.this,
                     "Please Wait", null, true, true);
         }
-
 
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
             progressDialog.dismiss();
-            String check = result;
-            System.out.println(result);
-            System.out.println(check);
-           // mTextViewResult.setText(result);
-            Log.d(TAG, "response  - " + result);
+            Log.d(TAG, "POST response  - " + result);
+            if(result==null){
 
-            if (result == null){
-
-               // mTextViewResult.setText(errorString);
-            }
-            else{
-                mJsonString =result;
-                check_login();
+            }else{
+                mJsonString = result;
+                check_signup();
             }
         }
 
-
         @Override
         protected String doInBackground(String... params) {
+            String id = (String)params[0];
+            String password = (String)params[1];
+            String company = (String)params[2];
+            String name = (String)params[3];
+            String phone = (String)params[4];
+            String tel = (String)params[5];
+            String email = (String)params[6];
+            String address = (String)params[7];
+            String imgurl = "url";
 
-            String user_id = (String)params[0];
-            String user_pw = (String)params[1];
-            String serverURL = "http://192.168.1.102/bcm/login.php";
-            String postParameters = "id=" + user_id + "&password=" + user_pw;
 
-            Log.v("왜 안되는것일까", user_id);
+            String serverURL = "http://192.168.1.102/bcm/signup.php";
+            String postParameters = "id=" + id +"&password=" + password +"&company=" + company + "&name=" + name + "&phone=" + phone + "&tel=" + tel +"&email=" + email + "&address=" + address+ "&imgurl=" + imgurl;
+
             try {
 
                 URL url = new URL(serverURL);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
 
                 httpURLConnection.setReadTimeout(5000);
                 httpURLConnection.setConnectTimeout(5000);
@@ -157,9 +139,8 @@ public class Login extends AppCompatActivity{
                 outputStream.flush();
                 outputStream.close();
 
-
                 int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d(TAG, "response code - " + responseStatusCode);
+                Log.d(TAG, "POST response code - " + responseStatusCode);
 
                 InputStream inputStream;
                 if(responseStatusCode == HttpURLConnection.HTTP_OK) {
@@ -169,35 +150,28 @@ public class Login extends AppCompatActivity{
                     inputStream = httpURLConnection.getErrorStream();
                 }
 
-
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
                 StringBuilder sb = new StringBuilder();
-                String line;
+                String line = null;
 
                 while((line = bufferedReader.readLine()) != null){
                     sb.append(line);
                 }
-
                 bufferedReader.close();
-                Log.d(TAG,sb.toString().trim());
                 return sb.toString().trim();
-
 
             } catch (Exception e) {
 
                 Log.d(TAG, "InsertData: Error ", e);
-                errorString = e.toString();
 
-                return null;
+                return new String("Error: " + e.getMessage());
             }
 
         }
     }
-
-
-    private void check_login(){
+    public void check_signup(){
         try {
             JSONObject jsonObject = new JSONObject(mJsonString);
             JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
@@ -207,25 +181,32 @@ public class Login extends AppCompatActivity{
             String check = item.getString(TAG_CHECK);
             switch (check){
                 case "success":
-                    Intent intent = new Intent(Login.this,MainActivity.class);
+                    Intent intent = new Intent(SignUp.this,Login.class);
                     startActivity(intent);
                     finish();
                     break;
-                case "fsql":
+                case "create_error":
+                case "insert_error":
                     Toast.makeText(getApplicationContext(),"서버에러",Toast.LENGTH_SHORT).show();
                     break;
-                case "fpwd":
+                case "already":
+                    Toast.makeText(getApplicationContext(),"이미 있는 아이디입니다.",Toast.LENGTH_SHORT).show();
+                    break;
                 case "fid":
                     Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호를 확인해주세요",Toast.LENGTH_SHORT).show();
                     break;
+                case "inputdata_error":
+                    Toast.makeText(getApplicationContext(),"에러",Toast.LENGTH_SHORT).show();
+                    break;
             }
+          if(check.equals("success")){
+              Intent intent = new Intent(SignUp.this,Login.class);
+              startActivity(intent);
+              finish();
+          }
         } catch (JSONException e) {
 
             Log.d(TAG, "showResult : ", e);
         }
-
     }
-
 }
-
-
